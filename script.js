0f602ae2cc39519feffe04e0fcea8b52b8a4e0fa
sachin kumar <merugune.sachin123@gmail.com>
2021-10-12 23:24:31 +0530

    Arithematic and assignment operations are performed using javascript

---
+++ script.js
@@ -0,0 +1,62 @@
+// Arithematic operators
+var num1 = 11
+var num2 = 10
+var num3 = 33
+
+//Addition
+console.log(num1+num2+num3)
+
+//subtraction
+console.log(num3-num1-num2)
+
+//multiplication
+console.log(num1 * num2)
+
+//division
+console.log(num3/num2)
+
+//modulus
+console.log(num3 % num1)
+console.log(num3 % num2)
+
+//increment
+console.log(num1++)
+console.log(++num2)
+console.log(num3++)
+
+// decrement
+console.log(num1--)
+console.log(--num3)
+console.log(num2--)
+
+//Assignment operators
+var num1 = 15
+var num2 = 24
+console.log(num1)
+
+num1+= 10
+console.log(num1)
+num2+=num1
+console.log(num2)
+
+num1-= 10
+console.log(num1)
+num2-=num1
+console.log(num2)
+
+
+num1*= 10
+console.log(num1)
+num2*=num1
+console.log(num2)
+
+
+num1/= 10
+console.log(num1)
+num2/=num1
+console.log(num2)
+
+num1%= 10
+console.log(num1)
+num2%=num1
+console.log(num2)
